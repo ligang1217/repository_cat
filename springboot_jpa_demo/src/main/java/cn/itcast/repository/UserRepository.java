245fd0dc7269ba40c6ebbeda564dc33368ab30ac
Tom <itheima@itcast.cn>
2019-01-10 00:55:58 +0800

    second commit add  findUserById

--- springboot_jpa_demo/src/main/java/cn/itcast/repository/UserRepository.java
+++ springboot_jpa_demo/src/main/java/cn/itcast/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User,Integer>,JpaSpecificationExecutor<User> {
 
     public List<User> findAll();
+
+    public User findUserById(Integer id);
 }
